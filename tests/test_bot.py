d07ae4ee03369846771c72fee3d3ae18a7051f5d
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2020-11-09 01:35:24 +0200

    added test example

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -3,9 +3,13 @@ import sys
 from pathlib import Path
 
 root = Path(__file__).parent.parent
-path = os.path.join(root, "notificator")
+path = os.path.join(root)
 sys.path.append(path)
 
-import commands_logic as cl  # noqa
+import notificator  # noqa
+from notificator import commands_logic as cl  # noqa
+
 
 # Write tests here
+def test_version():
+    assert notificator.__version__ == "0.7.0"
