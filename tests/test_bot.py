2f10677753995ee0c0cbe8245818c71997a46538
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2020-11-11 01:19:25 +0200

    changed version to 0.8.5

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -12,4 +12,4 @@ from notificator import commands_logic as cl  # noqa
 
 # Write tests here
 def test_version():
-    assert notificator.__version__ == "0.8.0"
+    assert notificator.__version__ == "0.8.5"
