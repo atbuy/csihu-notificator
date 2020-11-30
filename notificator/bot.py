4e686095b858bee0c3165a18c808e8a03e3e7944
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2020-11-30 14:44:17 +0200

    changing cooldown

--- notificator/bot.py
+++ notificator/bot.py
@@ -51,7 +51,7 @@ async def test(ctx: commands.Context) -> None:
 
 
 @client.command(name="kys", brief="Tell someone to kill themselves")
-@commands.cooldown(1, 45, commands.BucketType.channel)
+@commands.cooldown(2, 45, commands.BucketType.channel)
 async def kys(ctx: commands.Context, *, user: discord.User = None) -> None:
     """
     Paste's the user's image on another image and sends in on another image
