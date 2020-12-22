993d21234bebf3761d04a28e883fc6f99c0b53e4
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2020-12-23 00:21:55 +0200

    fixing typo

--- notificator/bot.py
+++ notificator/bot.py
@@ -141,7 +141,7 @@ async def get_icon(ctx: commands.Context, *, member: discord.Member = None) -> N
 
     if not member:
         url = ctx.author.avatar_url
-        await ctx.send(f"{ctx.author.mention} you icon is located at: {url}")
+        await ctx.send(f"{ctx.author.mention} your icon is located at: {url}")
     else:
         url = member.avatar_url
         await ctx.send(f"{ctx.author.mention}. This member's icon is located at: {url}")
