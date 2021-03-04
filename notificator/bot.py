952883f0a7efb65d30d27da6150ed73b2dfd5599
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2021-03-04 23:10:08 +0200

    fixed another typo

--- notificator/bot.py
+++ notificator/bot.py
@@ -1191,7 +1191,7 @@ async def mute(ctx: commands.Context, member: discord.Member, minutes: float = 5
 
     # One hour mute limit
     if minutes > 300:
-        await ctx.send(f"{ctx.author.mention} you can't mute someone for more than 1 hour.")
+        await ctx.send(f"{ctx.author.mention} you can't mute someone for more than 5 hours.")
         return
 
     # Check if the author can mute someone else
