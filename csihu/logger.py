c8e32c074ceec83127a5b61691a506bf16419689
Filip Ioannidis <filip1253@gmail.com>
2022-08-27 09:45:29 +0300

    Updated logging

--- csihu/logger.py
+++ csihu/logger.py
@@ -2,30 +2,6 @@ import logging
 import sys
 
 
-class Logger:
-    def __init__(self, level: int | None = logging.INFO):
-        # Setup logger
-        logger = logging.getLogger("CSIHU")
-        logger.setLevel(level)
-
-        # Setup formatter
-        formatting = "[%(asctime)s] [%(name)s] [%(levelname)s] :: %(message)s"
-        formatter = logging.Formatter(formatting, datefmt="%Y-%m-%d %H:%M:%S")
-
-        # Setup stream handler
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(formatter)
-
-        # Add handler to logger
-        logger.addHandler(handler)
-
-        self.logger = logger
-        self.level = level
-
-    def __call__(self, message: str, level: int | None = None) -> None:
-        self.log(message, level)
-
-
 def log(message: str, level: int | None = None) -> None:
     """Format message and log"""
 
@@ -38,3 +14,21 @@ def log(message: str, level: int | None = None) -> None:
     # Log message and save previous log
     logger.log(level, message)
     message = message
+
+
+def setup_logger(level: int | None = logging.INFO) -> None:
+    """Setup logger"""
+
+    logger = logging.getLogger("CSIHU")
+    logger.setLevel(level)
+
+    # Setup formatter
+    formatting = "[%(asctime)s] [%(name)s] [%(levelname)s] :: %(message)s"
+    formatter = logging.Formatter(formatting, datefmt="%Y-%m-%d %H:%M:%S")
+
+    # Setup stream handler
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+
+    # Add handler to logger
+    logger.addHandler(handler)
